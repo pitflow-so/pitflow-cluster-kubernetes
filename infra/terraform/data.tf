738b7b0f6cfc6782bd17ca5e3ed631e1186cf008
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-05-21 00:52:50 -0300

    feat: added ECR, EKS and autoscaler

---
+++ infra/terraform/data.tf
@@ -0,0 +1,28 @@
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  # Houve um erro então estou excluindo us-east-1e
+  filter {
+    name   = "availabilityZone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+  }
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.pitflow_cluster.name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.pitflow_cluster.name
+}
