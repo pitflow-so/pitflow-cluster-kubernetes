06ce68cf28a20b4760eb62dbc0affb65eae64ccb
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-05-21 16:20:02 -0300

    chore: removed sensitive atributo from vars

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -2,19 +2,16 @@ variable "eks_cluster_name" {
   description = "Nome do cluster EKS"
   type        = string
   default     = "pitflow-eks"
-  sensitive   = true
 }
 
 variable "eks_node_group_name" {
   description = "Nome do grupo de nós EKS"
   type        = string
   default     = "pitflow-node-group"
-  sensitive   = true
 }
 
 variable "ecr_repository_name" {
   description = "Nome do repositório ECR"
   type        = string
   default     = "pitflow-os-backend"
-  sensitive   = true
 }
